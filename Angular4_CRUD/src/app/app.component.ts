4e74ef581cc9502defe3171ae86d1d62e896bb17
Sweta Gupta <gsweta228@gmail.com>
2018-01-01 02:11:14 +0530

    changes for add, edit and delete funtionality

--- Angular4_CRUD/src/app/app.component.ts
+++ Angular4_CRUD/src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular CRUD Operation';
+  personData: any[] = [];
+  removePerson: boolean = true;
+  updatePersonDetails: boolean = false;
+  id = undefined;
+  user: object = {
+    id: "",
+    name: "",
+    age: ""
+  };
+
+  addPerson(portal) {
+    this.personData.push(portal.value);
+    if (this.personData.length) {
+      portal.reset();
+    }
+  }
+
+  deletePerson(id) {
+   this.personData.splice(id, 1);
+   
+  }
+
+  editPerson(data) {
+    this.user = data;
+    this.removePerson = false;
+    this.updatePersonDetails = true;
+  }
+  updatePerson(usergit) {
+    this.removePerson = true;
+    this.updatePersonDetails = false;
+    this.user = {};
+  }
 }
